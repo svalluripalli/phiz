ea01599d5b439efa49831b5501b9710ffc623bd9
Michal Kotelba <michal.kotelba@esacinc.com>
2015-06-07 17:45:18 -0400

    - Supports PHIZ-51. - Implemented missing service run directory creation during service startup.

--- phiz-core/src/main/scripts/groovy/phiz-process-resources-app.groovy
+++ phiz-core/src/main/scripts/groovy/phiz-process-resources-app.groovy
@@ -1,10 +1,17 @@
 import org.apache.commons.lang3.StringUtils
 
+def rootDirReplacePattern = "\\\${BASEDIR}/../../../"
+def defaultScriptFileReplacePattern = rootDirReplacePattern + "etc/default/\\\${APP_NAME}"
+
 ant.fileset(dir: "${project.properties["project.build.appassemblerDaemonsJswDirectory"]}/bin", erroronmissingdir: false, includes: project.artifactId).each{
+    def runDirReplacePattern = rootDirReplacePattern + project.properties["project.build.debRunDataDirectoryPath"]
+    
     it.file.write(it.file.text
         .replaceFirst(~/\n\[\s+\-f\s+".BASEDIR"\/bin\/setenv\-\$\{APP_NAME\}\.sh\s+\]/,
-            "\n[ -f \"\\\${BASEDIR}/../../../etc/default/\\\${APP_NAME}\" ] && . \"\\\${BASEDIR}/../../../etc/default/\\\${APP_NAME}\"\n\$0")
+            "\n[ -f \"${defaultScriptFileReplacePattern}\" ] && . \"${defaultScriptFileReplacePattern}\"\n\$0")
         .replaceFirst(~/(\n)#(RUN_AS_USER=)(\n)/, { it[1] + it[2] + project.artifactId + it[3] + "RUN_AS_GROUP=" + project.artifactId + it[3] })
+        .replaceFirst(~/\nPIDDIR="\.\.\/run"\n/,
+            "\$0\n[ -e \"${runDirReplacePattern}\" ] || { mkdir \"${runDirReplacePattern}\" && chown \"${project.artifactId}:${project.artifactId}\" \"${runDirReplacePattern}\"; }\n")
     )
 }
 
@@ -18,7 +25,7 @@ ant.fileset(dir: "${project.properties["project.build.appassemblerProgramsDirect
     it.file.write(it.file.text
         .replaceFirst(~/^(#!\/bin\/sh\n)(?:#[^\n]*\n)+/, "\$1\nAPP_NAME=\"${it.file.name}\"")
         .replaceFirst(~/\n\[\s+\-f\s+".BASEDIR"\/bin\/setenv\-\$\{APP_NAME\}\.sh\s+\]/,
-            "\n[ -f \"\\\${BASEDIR}/../../../etc/default/\\\${APP_NAME}\" ] && . \"\\\${BASEDIR}/../../../etc/default/\\\${APP_NAME}\"\n\$0")
+            "\n[ -f \"${defaultScriptFileReplacePattern}\" ] && . \"${defaultScriptFileReplacePattern}\"\n\$0")
         .replaceFirst(~/(\nexec\s+".JAVACMD"\s+.JAVA_OPTS)([^\n]+)(\s+\\\n)/, { it[1] + it[2].replaceAll(~/\s+([^\s]+)/, " \\\\\n  \$1") + it[3] })
         .replaceAll(~/(\s+\-D)(app\.[a-z]+)/, "\$1phiz.\$2")
     )
