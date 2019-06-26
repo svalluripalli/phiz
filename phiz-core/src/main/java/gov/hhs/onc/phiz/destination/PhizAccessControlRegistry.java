27fa08f31eb92df5aa5580b93cc4efe04d491595
Toan Le <toan.le@esacinc.com>
2019-06-26 11:42:19 -0400

    modified AccessControl DAO interfaces

--- phiz-core/src/main/java/gov/hhs/onc/phiz/destination/PhizAccessControlRegistry.java
+++ phiz-core/src/main/java/gov/hhs/onc/phiz/destination/PhizAccessControlRegistry.java
@@ -4,6 +4,7 @@ import gov.hhs.onc.phiz.data.db.PhizDataService;
 
 public interface PhizAccessControlRegistry extends PhizDataService<Integer, PhizAccessControl, PhizAccessControlDao> {
 
+    public Boolean checkSource(String sourceId);
     public Boolean checkDest(String sourceId, String destId);
 
 }
--- phiz-core/src/main/java/gov/hhs/onc/phiz/destination/PhizDestination.java
+++ phiz-core/src/main/java/gov/hhs/onc/phiz/destination/PhizDestination.java
@@ -15,4 +15,8 @@ public interface PhizDestination extends PhizEntity<String> {
     public String getPassword();
 
     public void setPassword(String password);
+
+    public String getVersion();
+
+    public void setVersion(String destVersion);
 }
